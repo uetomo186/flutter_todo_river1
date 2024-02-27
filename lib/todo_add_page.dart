9c3077ce6dd74712557a188ab2b6a0968f6521b0
uetomo186 <hagurono9@gmail.com>
2024-02-27 11:21:56 +0900

    入力フォームを追加

--- lib/todo_add_page.dart
+++ lib/todo_add_page.dart
@@ -8,13 +8,35 @@ class TodoAddPage extends StatelessWidget {
         title: const Text('リスト追加'),
       ),
       body: Container(
-        child: TextButton(
-          // ボタンをクリックした時の処理
-          onPressed: () {
-            // "pop"で前の画面に戻る
-            Navigator.of(context).pop();
-          },
-          child: const Text('リスト追加画面（クリックで戻る）'),
+        padding: const EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const TextField(),
+            const SizedBox(height: 8),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // リスト追加ボタン
+              child: ElevatedButton(
+                // ボタンをクリックした時の処理
+                onPressed: () {},
+                child: Text('リスト追加', style: TextStyle(color: Colors.black)),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              width: double.infinity,
+              child: TextButton(
+                // ボタンをクリックした時の処理
+                onPressed: () {
+                  // "pop"で前の画面に戻る
+                  Navigator.of(context).pop();
+                },
+                child: const Text('キャンセル'),
+              ),
+            ),
+          ],
         ),
       ),
     );
