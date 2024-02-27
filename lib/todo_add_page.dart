0b00fc40f1c1b0694f7c9d7d19db82d3e89992da
uetomo186 <hagurono9@gmail.com>
2024-02-27 13:25:31 +0900

    add

--- lib/todo_add_page.dart
+++ lib/todo_add_page.dart
@@ -1,27 +1,51 @@
 import 'package:flutter/material.dart';
 
-class TodoAddPage extends StatelessWidget {
+class TodoAddPage extends StatefulWidget {
   @override
+  _TodoAddPageState createState() => _TodoAddPageState();
+}
+
+class _TodoAddPageState extends State<TodoAddPage> {
+  String _text = '';
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('リスト追加'),
       ),
       body: Container(
-        padding: const EdgeInsets.all(64),
+        // 余白を付ける
+        padding: EdgeInsets.all(64),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const TextField(),
+            // 入力されたテキストを表示
+            Text(_text, style: TextStyle(color: Colors.blue)),
+            const SizedBox(height: 8),
+            // テキスト入力
+            TextField(
+              // 入力されたテキストの値を受け取る（valueが入力されたテキスト）
+              onChanged: (String value) {
+                // データが変更したことを知らせる（画面を更新する）
+                setState(() {
+                  // データを変更
+                  _text = value;
+                });
+              },
+            ),
             const SizedBox(height: 8),
             Container(
-              // 横幅いっぱいに広げる
               width: double.infinity,
-              // リスト追加ボタン
               child: ElevatedButton(
                 // ボタンをクリックした時の処理
-                onPressed: () {},
-                child: Text('リスト追加', style: TextStyle(color: Colors.black)),
+                onPressed: () {
+                  // "pop"で前の画面に戻る
+                  Navigator.of(context).pop(_text);
+                },
+                child: const Text(
+                  'リスト追加',
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
             ),
             const SizedBox(height: 8),
